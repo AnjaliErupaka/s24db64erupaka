ee6058e0e1ab5344ba4a0a6643963c3dc2225e4f
AnjaliErupaka <anjalisrinivas174@gmail.com>
2024-04-05 22:14:01 -0500

    add all

--- app.js
+++ app.js
@@ -1,11 +1,107 @@
-require('dotenv').config();
+// require('dotenv').config();
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// mongoose = require('mongoose');
+// var dog = require('./models/dogs')
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+// var dogRouter = require('./routes/dogs');
+// var gridRouter = require('./routes/grid');
+// var pickRouter = require('./routes/pick');
+// var resourceRouter = require('./routes/resource');
+
+// // We can seed the collection if needed on
+// //server start
+// async function recreateDB(){
+//   // Delete everything
+//   await dog.deleteMany();
+//   let instance1 = new
+//   dog({Dog_Breed:"German shepherd", Dog_Color:'black',
+//   Dog_Name: 5});
+//   instance1.save().then(doc=>{
+//   console.log("First object saved")}
+//   ).catch(err=>{
+//   console.error(err)
+//   });
+//   let instance2 = new
+//   dog({Dog_Breed:"Golden retriever", Dog_Color:'white',
+//   Dog_Name: 4});
+//   instance2.save().then(doc=>{
+//   console.log("First object saved")}
+//   ).catch(err=>{
+//   console.error(err)
+//   });
+//   let instance3 = new
+//   dog({Dog_Breed:"pug", Dog_Color:'brown',
+//   Dog_Name: 7});
+//   instance3.save().then(doc=>{
+//   console.log("First object saved")}
+//   ).catch(err=>{
+//   console.error(err)
+//   });
+//  }
+//  let reseed = true;
+//  if (reseed) {recreateDB();}
+ 
+// const connectionString = process.env.MONGO_CON;
+
+// var app = express();
+
+ 
+// mongoose.connect(connectionString);
+// //Get the default connection
+// var db = mongoose.connection;
+// //Bind connection to error event
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once("open", function(){
+// console.log("Connection to DB succeeded")});
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/dogs', dogRouter); 
+// app.use('/grid', gridRouter);
+// app.use('/pick', pickRouter);
+// app.use('/resource',resourceRouter);
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
+
+require('dotenv').config(); // Load environment variables from a .env file
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-mongoose = require('mongoose');
-var dog = require('./models/dogs')
+var mongoose = require('mongoose');
+var dog = require('./models/dogs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,50 +111,44 @@ var pickRouter = require('./routes/pick');
 var resourceRouter = require('./routes/resource');
 
 // We can seed the collection if needed on
-//server start
-async function recreateDB(){
+// server start
+async function recreateDB() {
   // Delete everything
   await dog.deleteMany();
-  let instance1 = new
-  dog({Dog_Breed:"German shepherd", Dog_Color:'black',
-  Dog_Name: 5});
-  instance1.save().then(doc=>{
-  console.log("First object saved")}
-  ).catch(err=>{
-  console.error(err)
+  let instance1 = new dog({ Dog_Breed: "German shepherd", Dog_Color: 'black', Dog_Name: 5 });
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }).catch(err => {
+    console.error(err)
   });
-  let instance2 = new
-  dog({Dog_Breed:"Golden retriever", Dog_Color:'white',
-  Dog_Name: 4});
-  instance2.save().then(doc=>{
-  console.log("First object saved")}
-  ).catch(err=>{
-  console.error(err)
+  let instance2 = new dog({ Dog_Breed: "Golden retriever", Dog_Color: 'white', Dog_Name: 4 });
+  instance2.save().then(doc => {
+    console.log("First object saved")
+  }).catch(err => {
+    console.error(err)
   });
-  let instance3 = new
-  dog({Dog_Breed:"pug", Dog_Color:'brown',
-  Dog_Name: 7});
-  instance3.save().then(doc=>{
-  console.log("First object saved")}
-  ).catch(err=>{
-  console.error(err)
+  let instance3 = new dog({ Dog_Breed: "pug", Dog_Color: 'brown', Dog_Name: 7 });
+  instance3.save().then(doc => {
+    console.log("First object saved")
+  }).catch(err => {
+    console.error(err)
   });
- }
- let reseed = true;
- if (reseed) {recreateDB();}
- 
+}
+let reseed = true;
+if (reseed) { recreateDB(); }
+
 const connectionString = process.env.MONGO_CON;
 
 var app = express();
 
- 
 mongoose.connect(connectionString);
-//Get the default connection
+// Get the default connection
 var db = mongoose.connection;
-//Bind connection to error event
+// Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -71,17 +161,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dogs', dogRouter); 
+app.use('/dogs', dogRouter);
 app.use('/grid', gridRouter);
 app.use('/pick', pickRouter);
-app.use('/resource',resourceRouter);
+app.use('/resource', resourceRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- controllers/dogs.js
+++ controllers/dogs.js
@@ -1,17 +1,28 @@
 var dog = require('../models/dogs');
 // List of all dogs
-exports.dog_list = function(req, res) {
- res.send('NOT IMPLEMENTED: dog list');
-};
-// for a specific dog.
-exports.dog_detail = async function(req, res) {
+// exports.dog_list = function(req, res) {
+//  res.send('NOT IMPLEMENTED: dog list');
+// };
+exports.dogs_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
     try {
-        const dog = await dog.findById(req.params.id);
-        res.render('dog', { title: 'dog Details', dog: dog });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+    result = await dogs.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
     }
-};
+    };
+    
+// for a specific dog.
+// exports.dog_detail = async function(req, res) {
+//     try {
+//         const dog = await dog.findById(req.params.id);
+//         res.render('dog', { title: 'dog Details', dog: dog });
+//     } catch (err) {
+//         res.status(500).json({ error: err.message });
+//     }
+// };
 // Handle dog create on POST.
 exports.dog_create_post = async function(req, res) {
     console.log(req.body)
@@ -19,7 +30,7 @@ exports.dog_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"Dog_Breed":"goat", "cost":12, "size":"large"}
     document.Dog_Breed = req.body.Dog_Breed;
     document.Dog_Color = req.body.Dog_Color;
     document.Dog_Name = req.body.Dog_Name;
@@ -43,14 +54,34 @@ exports.dog_delete = async function(req, res) {
     }
 };
 // Handle dog update form on PUT.
-exports.dog_update_put = async function(req, res) {
+// exports.dog_update_put = async function(req, res) {
+//     try {
+//         await dog.findByIdAndUpdate(req.params.id, req.body);
+//         res.json({ message: 'dog item updated successfully' });
+//     } catch (err) {
+//         res.status(500).json({ error: err.message });
+//     }
+// };
+
+exports.dogs_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
     try {
-        await dog.findByIdAndUpdate(req.params.id, req.body);
-        res.json({ message: 'dog item updated successfully' });
+    let toUpdate = await dogs.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.Dog_Breed)
+    toUpdate.Dog_Breed = req.body.Dog_Breed;
+    if(req.body.Dog_Color) toUpdate.Dog_Color = req.body.Dog_Color;
+    if(req.body.Dog_Name) toUpdate.Dog_Name = req.body.Dog_Name;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
     } catch (err) {
-        res.status(500).json({ error: err.message });
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
     }
-};
+    };
  
 // List of all dogs
 exports.dog_list = async function(req, res) {
